d4b01b0c331e017ca0f87b774f2d17ff798e2846
LuuucasM <127158177+LuuucasM@users.noreply.github.com>
2024-03-21 09:54:57 -0700

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "GLFW"
+	kind "StaticLib"
+	language "C++"
+  cppdialect "C++20"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+    **.h,
+    **.hpp,
+    **.cpp
+	}
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+		
+	filter "configurations:Dist"
+    runtime "Release"
+		optimize "on"
